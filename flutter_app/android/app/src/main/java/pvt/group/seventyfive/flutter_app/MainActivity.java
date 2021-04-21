55ca27d453dd4d229afbaac803c4e1ffd9ad0173
Robert Englund <kastanjen3@gmail.com>
2021-04-21 09:14:06 +0200

    flutter app and java server added

---
+++ flutter_app/android/app/src/main/java/pvt/group/seventyfive/flutter_app/MainActivity.java
@@ -0,0 +1,6 @@
+package pvt.group.seventyfive.flutter_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- java_server/src/pvt/group/seventyfive/java_server/Main.java
+++ java_server/src/pvt/group/seventyfive/java_server/Main.java
@@ -1,7 +1,8 @@
-package pvt.group.seventyfive;
+package pvt.group.seventyfive.java_server;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         System.exit(0);
     }
 }
