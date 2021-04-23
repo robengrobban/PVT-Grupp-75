36014c67a588d12eeaa202d18cfe8f10de7cf6f3
Johannesgyr <62520496+Johannesgyr@users.noreply.github.com>
2021-04-23 11:23:29 +0200

    Removed stuff, added test stuff
    temp

--- README.md
+++
@@ -1,8 +0,0 @@
-# PVT-Grupp-75
-Grupp 75 repository för PVT 7.5
-Camilla här
-Fredrik här
-Robert här
-Johannes här
-Jacob här
-Yaya här
--- flutter_app/android/app/src/main/java/pvt/group/seventyfive/flutter_app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package pvt.group.seventyfive.flutter_app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
