722c5ecb40a77d8bbdf69e3861467e1437c1a94a
Johannesgyr <62520496+Johannesgyr@users.noreply.github.com>
2021-04-23 12:18:15 +0200

    changed mainApplication
    hello -> good day

--- java_server/demo/src/main/java/com/example/demo/DemoApplication.java
+++ java_server/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -16,6 +16,6 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		return String.format("Good day, %s!", name);
 	}
 }
