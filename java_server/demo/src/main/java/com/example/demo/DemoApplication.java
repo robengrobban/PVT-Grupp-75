8ab2d6bc58c0430ad760dfff1b1aec6783f0c29f
casc7759 <camilla.bergman@telia.com>
2021-04-26 17:18:26 +0200

    testing Databaseconnection

--- java_server/demo/src/main/java/com/example/demo/DemoApplication.java
+++ java_server/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication extends SpringBootServletInitializer{
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
---
+++ java_server/demo/src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+	
+
+}
