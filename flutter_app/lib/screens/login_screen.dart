2a27abced962dfb9548e98b6fa489b33bdc9f5b8
Robert Englund <kastanjen3@gmail.com>
2021-04-27 12:56:16 +0200

    Update login_screen.dart

--- flutter_app/lib/screens/login_screen.dart
+++ flutter_app/lib/screens/login_screen.dart
@@ -35,7 +35,7 @@ class LoginScreenState extends State<LoginScreen> {
         appBar: AppBar(
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
-          title: Text("Hemsk inloggningsskärm"),
+          title: Text("Hemsökt inloggningsskärm"),
         ),
         backgroundColor: Colors.white,
         body: Column(
