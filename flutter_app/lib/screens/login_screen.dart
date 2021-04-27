025af8bcfab816b1e7a34930f7c587efa74f7327
Robert Englund <kastanjen3@gmail.com>
2021-04-27 12:56:36 +0200

    Update login_screen.dart

--- flutter_app/lib/screens/login_screen.dart
+++ flutter_app/lib/screens/login_screen.dart
@@ -33,8 +33,6 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text("Hemsökt inloggningsskärm"),
         ),
         backgroundColor: Colors.white,
