cb4039d9d3ef21dcc598d4121cc586a94db70bed
Robert Englund <kastanjen3@gmail.com>
2021-04-28 10:59:12 +0200

    Update login_screen.dart

--- flutter_app/lib/screens/login_screen.dart
+++ flutter_app/lib/screens/login_screen.dart
@@ -5,7 +5,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 GoogleSignIn _googleSignIn = GoogleSignIn(
   scopes: [
     'email',
-    'https://www.googleapis.com/auth/contacts.readonly',
+    'https://www.googleapis.com/auth/calendar.readonly',
   ],
 );
 
