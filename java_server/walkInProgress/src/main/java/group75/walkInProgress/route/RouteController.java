d6f2b80448f8487e6cda374ef8837342fa5ad2f2
CamillaSBergman <camilla.bergman@telia.com>
2021-04-30 18:27:26 +0200

    added parameters for request

--- java_server/walkInProgress/src/main/java/group75/walkInProgress/route/RouteController.java
+++ java_server/walkInProgress/src/main/java/group75/walkInProgress/route/RouteController.java
@@ -3,6 +3,7 @@ package group75.walkInProgress.route;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.maps.model.LatLng;
@@ -12,9 +13,9 @@ import com.google.maps.model.LatLng;
 public class RouteController {
 	private final RouteService service = new RouteService();
 	
-	  @GetMapping(path="/all")
-	  public @ResponseBody Route getAllUsers() {
-	    return service.getRoute(new LatLng(59.3380733384178, 18.061664496740775), 1);
+	  @GetMapping(path="/circularRoute")
+	  public @ResponseBody Route getCircularRoute(@RequestParam double lat, double lng, int duration, double radians) {
+	    return service.getRoute(new LatLng(lat, lng), duration, radians);
 	  }
 
 }
--- java_server/walkInProgress/src/main/java/group75/walkInProgress/route/RouteService.java
+++ java_server/walkInProgress/src/main/java/group75/walkInProgress/route/RouteService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.maps.*;
+import com.google.maps.DirectionsApi.RouteRestriction;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.LatLng;
 import com.google.maps.model.TravelMode;
@@ -14,14 +15,16 @@ public class RouteService {
 	final static int NUMBER_OF_WAYPOINTS = 3; 
 	
 
-	public Route getRoute(LatLng startPoint, double distance) {
-		var waypoints = generateWaypoints(startPoint, distance);
+	public Route getRoute(LatLng startPoint, double duration, double radians) {
+		var waypoints = generateWaypoints(startPoint, (duration/60)*5, radians);
 		
 		var req = DirectionsApi.newRequest(MapsContext.getInstance());
 		req.origin(startPoint);
 		req.destination(startPoint);
 		req.waypoints(waypoints.toArray(new LatLng[waypoints.size()]));
 		req.mode(TravelMode.WALKING);
+		req.avoid(RouteRestriction.FERRIES);
+		req.avoid(RouteRestriction.HIGHWAYS);
 		req.optimizeWaypoints(false);
 		try {
 			var result = req.await();
@@ -34,22 +37,20 @@ public class RouteService {
 		return null;
 	}
 	
-	private List<LatLng> generateWaypoints(LatLng startPoint, double distance) {
-		double angle = Math.random() * Math.PI * 2; 
+	private List<LatLng> generateWaypoints(LatLng startPoint, double distance, double radians) {
+		
 		List<LatLng> points = new ArrayList<LatLng>(); 
 		for (int i = 0; i < NUMBER_OF_WAYPOINTS; i++) { 
-			if ( i != 0) { 
-				angle += (60 * Math.PI / 180); 
-			} 
-		points.add(getPointOnCircumference( startPoint, distance / (NUMBER_OF_WAYPOINTS + 1), angle)); 
+			points.add(getPointOnCircumference( startPoint, distance / (NUMBER_OF_WAYPOINTS + 1), radians)); 
+			radians += (60 * Math.PI / 180); 
 		} 
 		return points;
 		
 	}
 
-	private LatLng getPointOnCircumference(LatLng startPoint, double legDistance, double angle) {
-		double latDifference = (Math.sin(angle) * legDistance) / 111;
-		double longDifference = (Math.cos(angle) * legDistance) / (Math.cos(startPoint.lat * Math.PI / 180) * 111.320); 
+	private LatLng getPointOnCircumference(LatLng startPoint, double legDistance, double radians) {
+		double latDifference = (Math.sin(radians) * legDistance) / 111;
+		double longDifference = (Math.cos(radians) * legDistance) / (Math.cos(startPoint.lat * Math.PI / 180) * 111.320); 
 		double newLat = startPoint.lat + latDifference; 
 		double newLong = startPoint.lng + longDifference; 
 		return new LatLng(newLat, newLong);
