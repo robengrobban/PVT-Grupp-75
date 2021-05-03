ea1ed0558b070d11e6ea4c0b9698eedde3c632b6
Robert Englund <kastanjen3@gmail.com>
2021-05-03 10:25:05 +0200

    Update account.dart

--- flutter_app/lib/models/account.dart
+++ flutter_app/lib/models/account.dart
@@ -103,11 +103,16 @@ class Account {
 
   Future<List<Event>> events() async {
     if ( !_loggedIn ) {
+      print("Error");
       return Future.error("Not logged in");
     }
-    if ( _lastEventsFetched == null || DateTime.now().difference(_lastEventsFetched).inSeconds > 30 ) {
+    if ( _lastEventsFetched == null || DateTime.now().difference(_lastEventsFetched).inMinutes > 30 ) {
       _events = await _generateCalendar();
       _lastEventsFetched = DateTime.now();
+      print("Update");
+    }
+    else {
+      print("Cache");
     }
     return _events;
   }
