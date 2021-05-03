636e85a7d05874006b528ba143e672f86ccd92ca
Robert Englund <kastanjen3@gmail.com>
2021-05-03 17:11:56 +0200

    Update AccountController.java

--- java_server/walkInProgress/src/main/java/group75/walkInProgress/account/AccountController.java
+++ java_server/walkInProgress/src/main/java/group75/walkInProgress/account/AccountController.java
@@ -2,13 +2,10 @@ package group75.walkInProgress.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 @RequestMapping(path="/account")
