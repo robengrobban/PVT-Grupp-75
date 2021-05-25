752e90f2bdc9b812a80702354c906335efe9cfaf
Yaya <yajaira.temesio@gmail.com>
2021-05-25 14:57:21 +0200

    Streaks förbättring

--- flutter_app/lib/models/Medal.dart
+++ flutter_app/lib/models/Medal.dart
@@ -1,24 +1,63 @@
+//import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Medal {
   MedalType type;
 
   Medal({this.type});
-
 }
 
 enum MedalType {
   THREE_DAY_STREAK,
-  FIVE_DAY_STREAK
+  FIVE_DAY_STREAK,
+  TEN_DAY_STREAK,
+  FIFTEEN_DAY_STREAK,
+  TWENTY_DAY_STREAK,
+  THIRTY_DAY_STREAK
 }
 
 extension ColorExtension on MedalType {
   Color get color {
     switch (this) {
       case MedalType.THREE_DAY_STREAK:
-        return Colors.red;
+        print("Three day streak");
+        return Colors.yellowAccent;
       case MedalType.FIVE_DAY_STREAK:
+        print("Five day streak");
         return Colors.blue;
+      case MedalType.TEN_DAY_STREAK:
+        print("Ten day streak");
+        return Colors.lightGreenAccent;
+      case MedalType.FIFTEEN_DAY_STREAK:
+        print("Fifteen day streak");
+        return Colors.red;
+      case MedalType.TWENTY_DAY_STREAK:
+        print("Twenty day streak");
+        return Colors.purpleAccent;
+      case MedalType.THIRTY_DAY_STREAK:
+        print("Thirty day streak");
+        return Colors.deepOrange;
+    }
+  }
+}
+
+extension TitleExtension on MedalType {
+  String get string {
+    switch (this) {
+      case MedalType.THREE_DAY_STREAK:
+        return "Three day streak";
+      case MedalType.FIVE_DAY_STREAK:
+        return "Five day streak";
+      case MedalType.TEN_DAY_STREAK:
+        return "Ten day streak";
+      case MedalType.FIFTEEN_DAY_STREAK:
+        return "Fifteen day streak";
+      case MedalType.TWENTY_DAY_STREAK:
+        return "Twenty day streak";
+      case MedalType.THIRTY_DAY_STREAK:
+        return "Thirty day streak";
     }
   }
 }
--- flutter_app/lib/screens/success_screen.dart
+++ flutter_app/lib/screens/success_screen.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:math';
 
+import 'package:calendar_widget/calendar_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/models/Medal.dart';
@@ -14,7 +16,6 @@ class SuccessScreen extends StatefulWidget {
 }
 
 class _SuccessScreenState extends State<SuccessScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -22,78 +23,116 @@ class _SuccessScreenState extends State<SuccessScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
       alignment: Alignment.center,
       decoration: BoxDecoration(gradient: Theme.appGradiant),
       child: DefaultTabController(
-          length: 2,
-          child: Scaffold(
+        length: 2,
+        child: Scaffold(
             appBar: AppBar(
-              centerTitle: true, title: Text("Achievements"),
+              centerTitle: true,
+              title: Text("Achievements"),
               bottom: TabBar(
-                labelStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                labelStyle:
+                    TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 indicatorSize: TabBarIndicatorSize.tab,
-              indicatorWeight: 10,
-              tabs: [
-                Tab(text:"Medals"),
-                Tab(text:"Streaks"),
-
-              ],
+                indicatorWeight: 10,
+                tabs: [
+                  Tab(text: "Awards"),
+                  Tab(text: "Streaks"),
+                ],
               ),
             ),
             body: Container(
               decoration: BoxDecoration(gradient: Theme.appGradiant),
               padding: EdgeInsets.all(32),
-              child:TabBarView(
-              children: [
-                MedalScreen(),
-                Icon(Icons.directions_transit),
-
-              ],
-            ),
-          )),
-        ),
+              child: TabBarView(
+                children: [
+                  MedalScreen(),
+                  StreakScreen(),
+                  //Icon(Icons.directions_transit),
+                ],
+              ),
+            )),
+      ),
     );
   }
 }
 
 List<Medal> _getMedals() {
-    return [
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.FIVE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-      new Medal(type: MedalType.THREE_DAY_STREAK),
-
-    ];
+  return [
+    new Medal(type: MedalType.THREE_DAY_STREAK),
+    new Medal(type: MedalType.FIVE_DAY_STREAK),
+    new Medal(type: MedalType.TEN_DAY_STREAK),
+    new Medal(type: MedalType.FIFTEEN_DAY_STREAK),
+    new Medal(type: MedalType.TWENTY_DAY_STREAK),
+    new Medal(type: MedalType.THIRTY_DAY_STREAK),
+    new Medal(type: MedalType.THREE_DAY_STREAK),
+    new Medal(type: MedalType.THREE_DAY_STREAK),
+    new Medal(type: MedalType.THREE_DAY_STREAK),
+    new Medal(type: MedalType.THREE_DAY_STREAK),
+    new Medal(type: MedalType.THREE_DAY_STREAK),
+    new Medal(type: MedalType.THREE_DAY_STREAK),
+    new Medal(type: MedalType.THREE_DAY_STREAK),
+    new Medal(type: MedalType.THREE_DAY_STREAK),
+    new Medal(type: MedalType.THREE_DAY_STREAK),
+  ];
 }
 
 class MedalScreen extends StatefulWidget {
   State createState() => _MedalScreenState();
 }
 
-class _MedalScreenState extends State<MedalScreen>{
+class _MedalScreenState extends State<MedalScreen> {
   @override
   Widget build(BuildContext context) {
     return GridView.count(
-      crossAxisCount: 3,
-      children: List.generate(_getMedals().length, (index) {
-        return Center(
-            child: Icon(Icons.local_police, color: _getMedals()[index].type.color,)
-        );
-      }
-      )
-    );
+        crossAxisCount: 3,
+        children: List.generate(_getMedals().length, (index) {
+          return Center(
+              child: ImageIcon(
+            AssetImage('assets/images/141054.png'),
+            size: 90,
+            color: _getMedals()[index].type.color,
+          ));
+        }));
   }
 }
 
+class StreakScreen extends StatefulWidget {
+  State createState() => _StreakScreenState();
+}
+
+class _StreakScreenState extends State<StreakScreen> {
+  @override
+  Widget build(BuildContext context) {
+    CalendarHighlighter highlighter = (DateTime dt) {
+      // randomly generate a boolean list of length monthLength + 1 (because months start at 1)
+      return List.generate(Calendar.monthLength(dt) + 1, (index) {
+        return (Random().nextDouble() < 0.3);
+      });
+    };
+    return Container(
+        margin: EdgeInsets.only(top: 16),
+        alignment: Alignment.topCenter,
+        child: Calendar(
+          width: MediaQuery.of(context).size.width - 32,
+          onTapListener: (DateTime dt) => showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                    title: const Text('Your route'),
+                    content: SingleChildScrollView(
+                        child: ListBody(
+                      children: const <Widget>[
+                        Text('Duration:'),
+                        Text('Distance:')
+                      ],
+                    )),
+                  )
+
+              //highlighter:highlighter,
+
+              ),
+        ));
+  }
+}
